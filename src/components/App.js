c5f8f8886fd226e760a56dd4aba6e5051228aaea
FNU Puneet <fnu.puneet@walmart.com>
2020-02-27 20:45:05 -0600

    Tested

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import Login from './Login';
 const App = () => (
     <Router>
         {/* Create a list of cities that we want to display on
-        dropdown and pass them as props to SelecCity component and render */}
+        dropdown and pass them as props to SelecCity component and render*/}
         <div className="App">
             {/* <SelectCity cityList={[{ label: 'Milwaukee', value: 'A0' },
                 { label: 'Chicago', value: 'B0' },
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,75 +1,64 @@
+import React, { useState } from 'react'
+import { GoogleLogout, GoogleLogin } from 'react-google-login'
+//import FontAwesome from 'react-fontawesome';
 
-import React, { useState } from 'react';
-import { GoogleLogout, GoogleLogin } from 'react-google-login';
-// import GoogleLogin, { GoogleLogout } from '../dist/google-login'
-// import FontAwesome from 'react-fontawesome';
-
-const clientId = '143791565531-bi6nbv001sqq9itv6uudo3a88h1llv6p.apps.googleusercontent.com';
+const clientId = '617246850621-95f9qhmehd380g2df86pjhrqc84n8nij.apps.googleusercontent.com'
 
 const success = response => {
   console.log(response) // eslint-disable-line
-};
+}
 
 const error = response => {
   console.error(response) // eslint-disable-line
-};
+}
 
 const loading = () => {
   console.log('loading') // eslint-disable-line
-};
-
-const logout = () => {
-  console.log('logout') // eslint-disable-line
-};
+}
 
 const MountTest = () => {
-    const [showButton, toggleShow] = useState(true);
+  const [showButton, toggleShow] = useState(true)
 
-    if (showButton) {
-        return (
-            <GoogleLogin
-                onSuccess={res => {
-                    toggleShow(false);
-                    success(res);
-                }}
-                onFailure={error}
-                clientId={clientId}
-            >
-        Auth then Hide button
-            </GoogleLogin>
-        );
-    }
+  if (showButton) {
+    return (
+      <GoogleLogin
+        onSuccess={res => {
+          toggleShow(false)
+          success(res)
+        }}
+        onFailure={error}
+        clientId={clientId}
+      >
+        Hide Button After Auth
+      </GoogleLogin>
+    )
+  }
 
-    return <button onClick={() => toggleShow(true)}>show button</button>;
-};
-const onClick = () => {
-    // ApI to validate if token is valid
-};
+  return <button onClick={() => toggleShow(true)}>show button</button>
+}
 
 export default () => (
-    <div>
-        {/* <MountTest /> */}
-        <br />
-        <br />
-        <GoogleLogin
-            clientId={clientId}
-            scope="https://www.googleapis.com/auth/analytics"
-            onSuccess={success}
-            onFailure={error}
-            onRequest={loading}
-            offline={false}
-            approvalPrompt="force"
-            responseType="id_token"
-            isSignedIn
-            theme="dark"
-            // disabled
-            // prompt="consent"
-            // className='button'
-            // style={{ color: 'red' }}
-        >
-            <span>Analytics</span>
-        </GoogleLogin>
-        <input id="RSAToken" type="text" name = "Token"></input>
-        <button onClick={this.onsubmit}></button>
-    </div>
-);
+  <div>
+    <MountTest />
+    <br />
+    <br />
+    <GoogleLogin
+      clientId={clientId}
+      scope="https://www.googleapis.com/auth/analytics"
+      onSuccess={success}
+      onFailure={error}
+      onRequest={loading}
+      offline={false}
+      approvalPrompt="force"
+      responseType="id_token"
+      isSignedIn
+      theme="dark"
+      // disabled
+      // prompt="consent"
+      // className='button'
+      // style={{ color: 'red' }}
+    >
+      <span>Login With Gmail</span>
+    </GoogleLogin>
+  </div>
+)
